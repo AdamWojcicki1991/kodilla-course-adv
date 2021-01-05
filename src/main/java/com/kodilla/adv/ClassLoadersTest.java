95f6406d4e515bad29d6513d0724377fb991afc0
Adam Wojcicki <adamwojcicki1991@gmail.com>
2021-01-05 10:53:31 +0100

    Add Kodilla exercises: Kodilla module 2.3

---
+++ src/main/java/com/kodilla/adv/ClassLoadersTest.java
@@ -0,0 +1,8 @@
+package com.kodilla.adv;
+
+public class ClassLoadersTest {
+    public static void main(String[] args) {
+        System.out.println(ClassLoadersTest.class.getClassLoader());
+        System.out.println(Throwable.class.getClassLoader());
+    }
+}

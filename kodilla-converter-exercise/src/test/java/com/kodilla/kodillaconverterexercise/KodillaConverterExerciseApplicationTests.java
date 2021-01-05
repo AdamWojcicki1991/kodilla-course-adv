97431b9f56d38542db550356386122e1a1b036c4
Adam Wojcicki <adamwojcicki1991@gmail.com>
2021-01-05 21:06:51 +0100

    Create Formatter from text to DTO: Kodilla Task 3.2

---
+++ kodilla-converter-exercise/src/test/java/com/kodilla/kodillaconverterexercise/KodillaConverterExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillaconverterexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaConverterExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
